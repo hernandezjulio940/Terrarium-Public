73b77402222648a491b5d9e45a90a857e4537a64
Peter Chan <peter9104@hotmail.com>
2017-02-18 13:08:35 +0800

    Update mvgee.js

--- mvgee.js
+++ mvgee.js
@@ -43,12 +43,12 @@ var MvGeeFirewallBypasser = {
 };
 
 //Extracted from the Firewall page (human verification)
-var hashFnc = function (a) {
-    for (var e, f, g, h, i, j, m, b = 0, c = 0, d = [], k = [e = 0x67452301, f = 0xEFCDAB89, ~e, ~f, 0xC3D2E1F0], l = [], n = decodeURI(encodeURI(a)), o = n.length; c <= o;)l[c >> 2] |= (n.charCodeAt(c) || 128) << 8 * (3 - c++ % 4);
-    for (l[m = o + 8 >> 2 | 15] = o << 3; b <= m; b += 16) {
-        for (e = k, c = 0; c < 80; e = [0 | [(j = ((n = e[0]) << 5 | n >>> 27) + e[4] + (d[c] = c < 16 ? ~~l[b + c] : j << 1 | j >>> 31) + 1518500248) + ((f = e[1]) & (g = e[2]) | ~f & (h = e[3])), i = j + (f ^ g ^ h) + 341375144, j + (f & g | f & h | g & h) + 982459459, i + 1535294389][0 | c++ / 20], n, f << 30 | f >>> 2, g, h])j = d[c - 3] ^ d[c - 8] ^ d[c - 14] ^ d[c - 16];
-        for (c = 5; c;)k[--c] = k[c] + e[c] | 0
-    }
-    for (a = ""; c < 40;)a += (k[c >> 3] >> 4 * (7 - c++ % 8) & 15).toString(16);
-    return a
-};
+var hashFnc = function(a) {
+        for (var e, f, g, h, i, j, m, b = 0, c = 0, d = [], k = [e = 0x67452301, f = 0xEFCDAB89, ~e, ~f, 0xC3D2E1F0], l = [], n = decodeURI(encodeURI(a)), o = n.length; c <= o;) l[c >> 2] |= (n.charCodeAt(c) || 128) << 8 * (3 - c++ % 4);
+        for (l[m = o + 8 >> 2 | 15] = o << 3; b <= m; b += 16) {
+            for (e = k, c = 0; c < 80; e = [0 | [(j = ((n = e[0]) << 5 | n >>> 27) + e[4] + (d[c] = c < 16 ? ~~l[b + c] : j << 1 | j >>> 31) + 1518500248) + ((f = e[1]) & (g = e[2]) | ~f & (h = e[3])), i = j + (f ^ g ^ h) + 341375144, j + (f & g | f & h | g & h) + 982459459, i + 1535294389][0 | c++/ 20], n, f << 30 | f >>> 2, g, h])j = d[c - 3] ^ d[c - 8] ^ d[c - 14] ^ d[c - 16];
+                        for (c = 5; c;) k[--c] = k[c] + e[c] | 0
+                    }
+                    for (a = ""; c < 40;) a += (k[c >> 3] >> 4 * (7 - c++ % 8) & 15).toString(16);
+                    return a
+                }
