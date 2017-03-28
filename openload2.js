b4606dcaec08ff7f10dfda5a7e70fe3d0adb2c46
Peter Chan <peter9104@hotmail.com>
2017-03-28 16:53:59 +0800

    Update openload2.js

--- openload2.js
+++ openload2.js
@@ -1,59 +1,22 @@
-//Created by NitroXenon
-//Github: https://github.com/NitroXenon/Terrarium-Public/blob/gh-pages/openload2.js
-//This script hooks all the browser functions used in the descryption script
-//eval() this script and you'll get the decoded url
-
-//The output decoded value
-var decodedValue = "";
-//The encoded string
-var encoded = "{encodedString}";
-//This is our custom variable of r. It can be any string value. (Original e.g.: dy72acuj8w)
-var r = "r";
-//Custom document object (because document only presents in browser)
+//Thank you @Tithen-Firion
+var id = "{encodedString}"
+var decoded = "";
 var document = {};
-//Custom jQuery stuff
-var $ = function(selector) {
-    if (selector == '#streamurl') {
-        //$("#streamurl") called
-        return {
-            //The final value is set by calling $("#streamurl").text(decodedValue)
-            //So we need to hook this function to get the result
-            //Result example: Djj9S0OU_XE~1490176146~14.199.0.0~P9V2fKPR
-            text: function(result) {
-                //$("#streamurl").text() called
-                result = 'https://openload.co/stream/' + result + '?mime=true';
-                decodedValue = result;
-            }
-        }
-    } else if (selector == '#' + r) {
-        return {
-            //The encoded string is queried
-            text: function() {
-                return encoded;
-            }
-        }
-    } else if (selector == document) {
-        return {
-            //Hook $(document).ready() because we're not in browser environment
-            ready: function(func) {
-                //$(document).ready() called
-                func();
-            }
-        }
-    } else {
-        //unknown selector called
+var window = this;
+var $ = function() {
+    return {
+        text: function(a) {
+            if (a)
+                decoded = a;
+            else
+                return id;
+        },
+        ready: function(a) {
+            a()
+        } 
     }
-}
+};
 
-/**
-  *
-  * The following is the decryption taken from the Openlaod embed webpage
-  *
-  */
+{decryptionScript};
 
-
-{decryptionScript}
-
-;
-
-decodedValue;
+decoded;
