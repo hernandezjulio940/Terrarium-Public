32bea8ba08a3b04c0d60a425a7dbb2ef25107637
Peter Chan <peter9104@hotmail.com>
2017-04-05 15:14:32 +0800

    v3

---
+++ openload3.js
@@ -0,0 +1,37 @@
+//Thank you @Tithen-Firion
+var id = "{encodedString}"
+var decoded = "";
+var document = {};
+var window = this;
+var $ = function() {
+    return {
+        text: function(a) {
+            if (a)
+                decoded = a;
+            else
+                return id;
+        },
+        ready: function(a) {
+            a()
+        }
+    }
+};
+
+(function(d) {
+    var f = function() {};
+    var s = '';
+    var o = null;
+    ['close', 'createAttribute', 'createDocumentFragment', 'createElement', 'createElementNS', 'createEvent', 'createNSResolver', 'createRange', 'createTextNode', 'createTreeWalker', 'evaluate', 'execCommand', 'getElementById', 'getElementsByName', 'getElementsByTagName', 'importNode', 'open', 'queryCommandEnabled', 'queryCommandIndeterm', 'queryCommandState', 'queryCommandValue', 'write', 'writeln'].forEach(function(e) {
+        d[e] = f;
+    });
+    ['anchors', 'applets', 'body', 'defaultView', 'doctype', 'documentElement', 'embeds', 'firstChild', 'forms', 'images', 'implementation', 'links', 'location', 'plugins', 'styleSheets'].forEach(function(e) {
+        d[e] = o;
+    });
+    ['URL', 'characterSet', 'compatMode', 'contentType', 'cookie', 'designMode', 'domain', 'lastModified', 'referrer', 'title'].forEach(function(e) {
+        d[e] = s;
+    });
+})(document);
+
+{decryptionScript};
+
+Duktape.enc("base64", decoded);
